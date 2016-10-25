e5580bddd0d9edbc774cd9f82a45106cd29fce2a
David Stainton <dstainton415@gmail.com>
2016-10-25 13:42:11 +0000

    Add simple benchmark

--- lioness_test.go
+++ lioness_test.go
@@ -31,3 +31,20 @@ func TestBasicLionessEncrypt(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func BenchmarkLioness(b *testing.B) {
+	key := make([]byte, lionessKeyLen)
+	for i := 0; i < lionessKeyLen; i++ {
+		key[i] = byte(i) & 0xff
+	}
+
+	plaintext := []byte("'What do we know,' he had said, 'of the world and the universe about us? Our means of receiving impressions are absurdly few, and our notions of surrounding objects infinitely narrow. We see things only as we are constructed to see them, and can gain no idea of their absolute nature. With five feeble senses we pretend to comprehend the boundlessly complex cosmos, yet other beings with wider, stronger, or different range of senses might not only see very differently the things we see, but might see and st")
+	cipher := NewLionessCipher(key, len(plaintext)) // key and block-size
+
+	for i := 0; i < b.N; i++ {
+		_, err := cipher.Encrypt(plaintext)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
