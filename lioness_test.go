b6c37ec946987df45ac0d72510f3728fe06883c8
David Stainton <dstainton415@gmail.com>
2016-12-12 23:54:39 +0000

    Minor corrections to benchmark test

--- lioness_test.go
+++ lioness_test.go
@@ -167,15 +167,17 @@ func BenchmarkLioness(b *testing.B) {
 	plaintext := []byte("Open, secure and reliable connectivity is necessary (although not   sufficient) to excercise the human rights such as freedom of   expression and freedom of association [FOC], as defined in the   Universal Declaration of Human Rights [UDHR].  The purpose of the   Internet to be a global network of networks that provides unfettered   connectivity to all users and for any content [RFC1958].  This   objective of stimulating global connectivity contributes to the   Internet's role as an enabler of human rights.  Next to that, the   strong commitment to security [RFC1984] [RFC3365] and privacy   [RFC6973] [RFC7258] in the Internet's architectural design contribute   to the strengthening of the Internet as a human rights enabling   environment.  One could even argue that the Internet is not only an   enabler of human rights, but that human rights lie at the basis of,   and are ingrained in, the architecture of the network.  Internet   connectivity increases the capacity for individuals to exercise their   rights, the core of the Internet, its architectural design is   therefore closely intertwined with the human rights framework   [CathFloridi].  The quintessential link between the Internet's   architecture and human rights has been argued by many.  [Bless] for   instance argues that, 'to a certain extent, the Internet and its   protocols have already facilitated the realization of human rights,   e.g., the freedom of assembly and expression.  In contrast, measures   of censorship and pervasive surveillance violate fundamental human   rights.'  [Denardis15] argues that 'Since the first hints of Internet   commercialization and internationalization,")
 	cipher, err := NewCipher(key, len(plaintext)) // key and block-size
 	if err != nil {
-		panic(err)
+		b.Fatal()
 	}
 
 	var ciphertext []byte
 	for i := 0; i < b.N; i++ {
 		ciphertext, err = cipher.Encrypt(plaintext)
+		b.StopTimer()
 		if err != nil {
-			panic(err)
+			b.Fatal()
 		}
+		b.StartTimer()
 	}
 
 	// always store the result to a package level variable
