b8a62eb688e9d7762f6af3aa2d28d02764c4c45b
David Stainton <dstainton415@gmail.com>
2016-12-23 00:13:53 +0000

    Use zero nonce for chacha20 + add verified test vectors
    test vectors verified with our rust lioness here:
    https://github.com/burdges/lioness-rs

--- lioness.go
+++ lioness.go
@@ -10,21 +10,17 @@ import (
 
 const (
 	// KeyLen is the length of our Lioness key
-	KeyLen         = 208
-	chachaNonceLen = 8
-	chachaKeyLen   = 32
-	hashKeyLen     = 64 // blake2b key len
-	secretKeyLen   = chachaKeyLen + chachaNonceLen
-	// MinBlockSize is the minimum block size the cipher can process
-	MinBlockSize = secretKeyLen
+	KeyLen             = 2*streamCipherKeyLen + 2*hashKeyLen
+	streamCipherKeyLen = 32
+	hashKeyLen         = 64 // blake2b key len
 )
 
 // Cipher allows you to encrypt/decrypt large blocks
 type Cipher struct {
 	blockSize int
-	k1        [secretKeyLen]byte
+	k1        [streamCipherKeyLen]byte
 	k2        [hashKeyLen]byte
-	k3        [secretKeyLen]byte
+	k3        [streamCipherKeyLen]byte
 	k4        [hashKeyLen]byte
 }
 
@@ -32,16 +28,16 @@ type Cipher struct {
 func NewCipher(key [KeyLen]byte, blockSize int) (*Cipher, error) {
 	// The block size must accomodate |L| = S_KEY_LEN, along with
 	// |R| > 0, and the key should be the correct size.
-	if blockSize <= MinBlockSize {
-		return nil, fmt.Errorf("LIONESS block size mismatch error: %d <= %d min block size", blockSize, MinBlockSize)
+	if blockSize <= KeyLen {
+		return nil, fmt.Errorf("LIONESS block size mismatch error: %d <= %d min block size", blockSize, KeyLen)
 	}
 	c := Cipher{
 		blockSize: blockSize,
 	}
-	copy(c.k1[:], key[:secretKeyLen])
-	copy(c.k2[:], key[secretKeyLen:secretKeyLen+hashKeyLen])
-	copy(c.k3[:], key[secretKeyLen+hashKeyLen:secretKeyLen*2+hashKeyLen])
-	copy(c.k4[:], key[(2*secretKeyLen+hashKeyLen):hashKeyLen+(2*secretKeyLen+hashKeyLen)])
+	copy(c.k1[:], key[:streamCipherKeyLen])
+	copy(c.k2[:], key[streamCipherKeyLen:streamCipherKeyLen+hashKeyLen])
+	copy(c.k3[:], key[streamCipherKeyLen+hashKeyLen:streamCipherKeyLen*2+hashKeyLen])
+	copy(c.k4[:], key[(2*streamCipherKeyLen+hashKeyLen):hashKeyLen+(2*streamCipherKeyLen+hashKeyLen)])
 	return &c, nil
 }
 
@@ -51,7 +47,7 @@ func (c *Cipher) Encrypt(block []byte) ([]byte, error) {
 		return nil, errors.New("LIONESS Encrypt failed: input block size is not equal to block size")
 	}
 
-	lSize := secretKeyLen
+	lSize := streamCipherKeyLen
 	rSize := c.blockSize - lSize
 	tmp := make([]byte, lSize)
 	l := make([]byte, lSize)
@@ -60,7 +56,8 @@ func (c *Cipher) Encrypt(block []byte) ([]byte, error) {
 
 	// R = R ^ S(L ^ K1)
 	XorBytes(tmp, block[:lSize], c.k1[:])
-	chacha, err := chacha20.NewCipher(tmp[chachaNonceLen:chachaNonceLen+chachaKeyLen], tmp[:chachaNonceLen])
+	var zeroNonce [8]byte
+	chacha, err := chacha20.NewCipher(tmp[:streamCipherKeyLen], zeroNonce[:])
 	if err != nil {
 		return nil, fmt.Errorf("LIONESS Encrypt failed: %v", err)
 	}
@@ -75,7 +72,7 @@ func (c *Cipher) Encrypt(block []byte) ([]byte, error) {
 
 	// R = R ^ S(L ^ K3)
 	XorBytes(tmp, l, c.k3[:])
-	chacha, err = chacha20.NewCipher(tmp[chachaNonceLen:chachaNonceLen+chachaKeyLen], tmp[:chachaNonceLen])
+	chacha, err = chacha20.NewCipher(tmp[:streamCipherKeyLen], zeroNonce[:])
 	if err != nil {
 		return nil, fmt.Errorf("LIONESS Encrypt failed: %v", err)
 	}
@@ -101,7 +98,7 @@ func (c *Cipher) Decrypt(block []byte) ([]byte, error) {
 		return nil, errors.New("LIONESS Decrypt failed: input block size is not equal to block size")
 	}
 
-	lSize := secretKeyLen
+	lSize := streamCipherKeyLen
 	rSize := c.blockSize - lSize
 	tmp := make([]byte, lSize)
 	l := make([]byte, lSize)
@@ -117,7 +114,8 @@ func (c *Cipher) Decrypt(block []byte) ([]byte, error) {
 
 	// R = R ^ S(L ^ K3)
 	XorBytes(tmp, l, c.k3[:])
-	chacha, err := chacha20.NewCipher(tmp[chachaNonceLen:chachaNonceLen+chachaKeyLen], tmp[:chachaNonceLen])
+	var zeroNonce [8]byte
+	chacha, err := chacha20.NewCipher(tmp[:streamCipherKeyLen], zeroNonce[:])
 	if err != nil {
 		return nil, fmt.Errorf("LIONESS Decrypt failed: %v", err)
 	}
@@ -132,7 +130,7 @@ func (c *Cipher) Decrypt(block []byte) ([]byte, error) {
 
 	// R = R ^ S(L ^ K1)
 	XorBytes(tmp, l, c.k1[:])
-	chacha, err = chacha20.NewCipher(tmp[chachaNonceLen:chachaNonceLen+chachaKeyLen], tmp[:chachaNonceLen])
+	chacha, err = chacha20.NewCipher(tmp[:streamCipherKeyLen], zeroNonce[:])
 	if err != nil {
 		return nil, fmt.Errorf("LIONESS Decrypt failed: %v", err)
 	}
